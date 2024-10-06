b017644d0cb033e7b32e24c4551d9d7503d3db15
WCM-CS <walker.educs@gmail.com>
2024-10-06 18:27:52 -0400

    Added Pseudo code for the cli, rough draft/ideas

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 
 // Store values in struct
 
-// Unkown section -- Either we use conditions to determine which protein sources to load or choopse at random then potentially have to select the other optioon whihc would require additional calls to the proxy,
+// Unkown section -- Either we use conditions to determine which protein sources to load or choose at random then potentially have to select the other optioon which would require additional calls to the proxy,
 // To avoid additonal calls potentially set a fat limit such that if fat macro is under x we know we must choose low fat protein option 
 
 // Define the implimentation files to load the values into the structs per each type of food 
@@ -16,7 +16,7 @@
 
 // Based on the foods selected via random number generaton related to length of the vector store the index of the structs from their vector in another, clean up previous vectors for memory reasons
 
-// NOTE: potential error if we chose wrong food options since going back at this pout would require loading all values again aka starting over
+// NOTE: potential error if we chose wrong food options since going back at this point would require loading all values again aka starting over
 
 // Once foods are selected interact with the proxy sending the data through to gather the nutrient valies of the given foods we selected
 
